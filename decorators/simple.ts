5a7c2c22af86164894c930b019c68fd7ba6010bb
alberto.arroyo <beyondnet.peru@gmail.com>
2023-02-25 18:25:48 -0500

    adding decorator types sample

--- decorators/simple.ts
+++ decorators/simple.ts
@@ -1,14 +1,33 @@
-function sealed(construction: Function) {
-  console.log('Class is getting executed');
+function classDecorator(target: object) {
+  console.log('Teacher class accessed');
 }
 
-@sealed
-export class Student {
-  constructor() {
-    console.log('Object created');
+function propertyDecorator(target: object, propertyKey: string) {
+  console.log('Teacher skill property accessed');
+}
+
+function methodDecorator(target: object, propertyKey: string, descriptot: PropertyDescriptor) {
+  console.log('Teacher research method accessed');
+}
+
+function parameterDecorator(target: object, propertyKey: string, parameterIndex: number) {
+  console.log('Teacher research parameter method accessed');
+}
+
+@classDecorator
+class Teacher {
+  @propertyDecorator
+  _skill: string;
+  constructor(readonly name: string) {}
+
+  @methodDecorator
+  research(@parameterDecorator topic: string) {
+    console.log('Researching ' + topic);
   }
 }
 
 export const Main = () => {
-  let student = new Student();
+  const teacher = new Teacher('Beto');
+  teacher._skill = 'JS';
+  teacher.research('Decorators');
 };
